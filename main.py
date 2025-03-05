53069fd514af3665ec6d71412c7e457708a86903
polohot <polohot@gmail.com>
2025-03-05 21:50:25 +0700

    adding single question

--- main.py
+++ main.py
@@ -11,18 +11,38 @@ async def root():
     return {"message": "Hello World"}
 
 @app.post("/openai")
-async def openai_connector(
-    apikey: str,
-    body: dict):
-
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {apikey}"
-    }
-
+async def openai(apikey: str, body: dict):
+    # INIT HEADER
+    headers = {"Content-Type": "application/json",
+               "Authorization": f"Bearer {apikey}"}
+    # CALL API
     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=body)
-
     return response.json()
+
+@app.post("/openai_single")
+async def openai_single(apikey: str, question: str):
+    # INIT HEADER
+    headers = {"Content-Type": "application/json",
+               "Authorization": f"Bearer {apikey}"}
+    # BUILD BODY    
+    body = {"model": "gpt-4o",
+            "messages": [{'role': 'user', 
+                          'content': question}],
+            "max_tokens": 1000}
+    # CALL API
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=body)
+    return response.json()
+
+
+
+# @app.post("/deepseek")
+# async def deepseek(apikey: str, body: dict):
+#     # INIT HEADER
+#     headers = {"Content-Type": "application/json",
+#                "Authorization": f"Bearer {apikey}"}
+#     # CALL API
+#     response = requests.post("https://api.deepseek.com/v1/chat/completions", headers=headers, json=body)
+#     return response.json()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8080)
